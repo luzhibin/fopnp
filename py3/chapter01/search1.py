1bf2d63d18852fb93e1b21fd70bc65ef079328cc
Puppy <44298549+luzhibin@users.noreply.github.com>
2019-12-24 17:22:19 +0800

    Update search1.py

--- py3/chapter01/search1.py
+++ py3/chapter01/search1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Foundations of Python Network Programming, Third Edition
 # https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter01/search1.py
+# 代码清单1-1  获取经度与纬度
 
 # (The Google API originally used in this example now requires API keys,
 #  so here's an alternative that calls openstreetmap.org.)
