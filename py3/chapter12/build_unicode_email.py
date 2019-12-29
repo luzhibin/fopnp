37ac33cd6addffa13fb7bb79e5f5e4f8b4509a21
Puppy <44298549+luzhibin@users.noreply.github.com>
2019-12-29 23:38:31 +0800

    代码清单12-6 生成提供国际化支持的电子邮件，用于测试代码清单12-5中的解析脚本

--- py3/chapter12/build_unicode_email.py
+++ py3/chapter12/build_unicode_email.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Foundations of Python Network Programming, Third Edition
 # https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter12/build_unicode_email.py
+# 代码清单12-6 生成提供国际化支持的电子邮件，用于测试代码清单12-5中的解析脚本
 
 import email.message, email.policy, sys
 
