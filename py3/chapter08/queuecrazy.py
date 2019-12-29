90b8da8a19ad1a58ec890aceabb4e08a26db19ee
Puppy <44298549+luzhibin@users.noreply.github.com>
2019-12-29 23:12:58 +0800

    代码清单8-3 连接5个不同模块的MQ消息机制

--- py3/chapter08/queuecrazy.py
+++ py3/chapter08/queuecrazy.py
@@ -2,6 +2,7 @@
 # actually named "queuepi.py", as you can see a page or two later when
 # the book demonstrates how to invoke the script at the shell prompt.
 # So the script actually lives at the URL:
+# 代码清单8-3 连接5个不同模块的MQ消息机制
 
 # https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter08/queuepi.py
 
