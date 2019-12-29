7715a18d3f7c33e86109c486ad91106415d15744
Puppy <44298549+luzhibin@users.noreply.github.com>
2019-12-29 23:00:20 +0800

    代码清单7-11 对一个或多个客户端连接做出响应，最终发生超时

--- py3/chapter07/in_zen2.py
+++ py3/chapter07/in_zen2.py
@@ -2,6 +2,7 @@
 # Foundations of Python Network Programming, Third Edition
 # https://github.com/brandon-rhodes/fopnp/blob/m/py3/chapter07/in_zen2.py
 # Multi-shot server for the use of inetd(8).
+# 代码清单7-11 对一个或多个客户端连接做出响应，最终发生超时
 
 import socket, sys, zen_utils
 
